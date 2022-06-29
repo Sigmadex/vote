3fa6b64b2aaed98faf39a571995dd1273944a129
Jaf Crisologo <rtql8d@gmail.com>
2022-06-29 12:12:29 -0600

    updates styling to match Figma file

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -8,7 +8,8 @@ import { Routes, Route, Link } from 'react-router-dom'
 
 function App() {
   return (
-    <div className='App'>
+    // <div className='App'>
+    <div>
       {/* <Alert /> */}
       <Navbar />
 
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import ConnectButton from '../ConnectButton'
 
-const Logo = () => <img alt='Sigmadex logo' src='/images/Sigmadex-logo.svg' />
+const Logo = () => <img style={{cursor: 'pointer'}} alt='Sigmadex logo' src='/images/Sigmadex-logo.svg' />
+
+const navbarStyles = {
+  height: '90px',
+  // paddingTop: '90px',
+  paddingLeft: '36px',
+  paddingRight: '36px',
+  display: 'flex',
+  alignItems: 'center',
+  position: 'fixed',
+  top: '0px',
+  left: '0px',
+  right: '0px',
+  display: 'flex',
+  justifyContent: 'space-between'
+}
 
 const Header = () => {
   return (
-    <>
+    <div style={navbarStyles}>
       <Logo />
-      <img alt='AVAX logo' src='/images/avax-logo-white.svg' />
-      <ConnectButton />
-    </>
+      <span>
+        <img alt='AVAX logo' src='/images/avax-logo-white.svg' />
+        <span style={{paddingLeft: '26px'}}>
+          <ConnectButton />
+        </span>
+      </span>
+    </div>
   );
 }
 
