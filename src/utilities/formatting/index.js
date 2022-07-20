9b52e27e64ce8215f353f256f8c7f722dbb7dfd5
Jaf Crisologo <rtql8d@gmail.com>
2022-07-20 16:26:41 -0600

    sets up boilerplate for Ballot.sol (from Remix), adds helper functions for converting between 32 bytes and strings

--- src/utilities/formatting/index.js
+++ src/utilities/formatting/index.js
@@ -1,7 +1,20 @@
 /*
   MARK: - All data formatting methods go here
 */
+import { ethers } from 'ethers'
 
 export const truncateAddress = (address) => {
   return address.substring(0, 6) + '...' + address.substring(address.length - 4)
+}
+
+export async function createBytes(args) {
+  let name = args[0]
+  let bytes = ethers.utils.formatBytes32String(name)
+  return bytes
+}
+
+export async function parseBytes(args) {
+  let bytes = args[0]
+  let name = await ethers.utils.parseBytes32String(bytes)
+  return name
 }
