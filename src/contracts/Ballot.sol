8946905a94e40e5812e82593121bce3dd1ccdddc
Jaf Crisologo <rtql8d@gmail.com>
2023-01-02 17:18:33 -0700

    deletes unused contracts, updates Ballot contract

--- src/contracts/Ballot.sol
+++ src/contracts/Ballot.sol
@@ -1,89 +1,98 @@
 // SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.4;
 
-pragma solidity >=0.7.0 <0.9.0;
-
-/**
- * @title Ballot
- * @dev Implements voting process along with vote delegation
- */
+/// @title Voting with delegation.
 contract Ballot {
-
+    // This declares a new complex type which will
+    // be used for variables later.
+    // It will represent a single voter.
     struct Voter {
-        uint weight; // weight is accumulated by delegation (defaults to 0)
-        bool voted;  // if true, that person already voted
+        uint256 weight; // weight is accumulated by delegation
+        bool voted; // if true, that person already voted
         address delegate; // person delegated to
-        uint vote;   // index of the voted proposal
+        uint256 vote; // index of the voted proposal
     }
 
+    // This is a type for a single proposal.
     struct Proposal {
-        // If you can limit the length to a certain number of bytes,
-        // always use one of bytes1 to bytes32 because they are much cheaper
-        bytes32 name;   // short name (up to 32 bytes)
-        // string stringName; // uses more gas
-        uint voteCount; // number of accumulated votes
-        // TODO: - How to handle multiple options?
+        bytes32 name; // short name (up to 32 bytes)
+        uint256 voteCount; // number of accumulated votes
     }
 
     address public chairperson;
 
+    // This declares a state variable that
+    // stores a `Voter` struct for each possible address.
     mapping(address => Voter) public voters;
 
+    // A dynamically-sized array of `Proposal` structs.
     Proposal[] public proposals;
 
-    /**
-     * @dev Create a new ballot to choose one of 'proposalNames'.
-     * @param proposalNames names of proposals
-     */
+    /// Create a new ballot to choose one of `proposalNames`.
     constructor(bytes32[] memory proposalNames) {
         chairperson = msg.sender;
         voters[chairperson].weight = 1;
 
-        for (uint i = 0; i < proposalNames.length; i++) {
-            // 'Proposal({...})' creates a temporary
-            // Proposal object and 'proposals.push(...)'
-            // appends it to the end of 'proposals'.
-            proposals.push(Proposal({
-                name: proposalNames[i],
-                voteCount: 0
-            }));
+        // For each of the provided proposal names,
+        // create a new proposal object and add it
+        // to the end of the array.
+        for (uint256 i = 0; i < proposalNames.length; i++) {
+            // `Proposal({...})` creates a temporary
+            // Proposal object and `proposals.push(...)`
+            // appends it to the end of `proposals`.
+            proposals.push(Proposal({name: proposalNames[i], voteCount: 0}));
         }
     }
 
-    /**
-     * @dev Give 'voter' the right to vote on this ballot. May only be called by 'chairperson'.
-     * @param voter address of voter
-     */
+    // Give `voter` the right to vote on this ballot.
+    // May only be called by `chairperson`.
     function giveRightToVote(address voter) public {
+        // If the first argument of `require` evaluates
+        // to `false`, execution terminates and all
+        // changes to the state and to Ether balances
+        // are reverted.
+        // This used to consume all gas in old EVM versions, but
+        // not anymore.
+        // It is often a good idea to use `require` to check if
+        // functions are called correctly.
+        // As a second argument, you can also provide an
+        // explanation about what went wrong.
         require(
             msg.sender == chairperson,
             "Only chairperson can give right to vote."
         );
-        require(
-            !voters[voter].voted,
-            "The voter already voted."
-        );
+        require(!voters[voter].voted, "The voter already voted.");
         require(voters[voter].weight == 0);
-        voters[voter].weight = 1; // think of this as number of votes
+        voters[voter].weight = 1;
     }
 
-    /**
-     * @dev Delegate your vote to the voter 'to'.
-     * @param to address to which vote is delegated
-     */
-    // essentially giving someone else the ability to vote on your behalf
+    /// Delegate your vote to the voter `to`.
     function delegate(address to) public {
-        Voter storage sender = voters[msg.sender]; // the current voter
+        // assigns reference
+        Voter storage sender = voters[msg.sender];
         require(!sender.voted, "You already voted.");
+
         require(to != msg.sender, "Self-delegation is disallowed.");
 
+        // Forward the delegation as long as
+        // `to` also delegated.
+        // In general, such loops are very dangerous,
+        // because if they run too long, they might
+        // need more gas than is available in a block.
+        // In this case, the delegation will not be executed,
+        // but in other situations, such loops might
+        // cause a contract to get "stuck" completely.
         while (voters[to].delegate != address(0)) {
             to = voters[to].delegate;
 
             // We found a loop in the delegation, not allowed.
             require(to != msg.sender, "Found loop in delegation.");
         }
+
+        // Since `sender` is a reference, this
+        // modifies `voters[msg.sender].voted`
         sender.voted = true;
-        sender.delegate = to; // "to" is who is being delegated to
+        sender.delegate = to;
         Voter storage delegate_ = voters[to];
         if (delegate_.voted) {
             // If the delegate already voted,
@@ -96,33 +105,26 @@ contract Ballot {
         }
     }
 
-    /**
-     * @dev Give your vote (including votes delegated to you) to proposal 'proposals[proposal].name'.
-     * @param proposal index of proposal in the proposals array
-     */
-    function vote(uint proposal) public { // proposal is the index number (0th-index)
+    /// Give your vote (including votes delegated to you)
+    /// to proposal `proposals[proposal].name`.
+    function vote(uint256 proposal) public {
         Voter storage sender = voters[msg.sender];
         require(sender.weight != 0, "Has no right to vote");
         require(!sender.voted, "Already voted.");
         sender.voted = true;
         sender.vote = proposal;
 
-        // If 'proposal' is out of the range of the array,
+        // If `proposal` is out of the range of the array,
         // this will throw automatically and revert all
         // changes.
         proposals[proposal].voteCount += sender.weight;
     }
 
-    /**
-     * @dev Computes the winning proposal taking all previous votes into account.
-     * @return winningProposal_ index of winning proposal in the proposals array
-     */
-    function winningProposal() public view
-            returns (uint winningProposal_)
-    {
-        // TODO: - How to handle proposals with the same tallies?
-        uint winningVoteCount = 0;
-        for (uint p = 0; p < proposals.length; p++) {
+    /// @dev Computes the winning proposal taking all
+    /// previous votes into account.
+    function winningProposal() public view returns (uint256 winningProposal_) {
+        uint256 winningVoteCount = 0;
+        for (uint256 p = 0; p < proposals.length; p++) {
             if (proposals[p].voteCount > winningVoteCount) {
                 winningVoteCount = proposals[p].voteCount;
                 winningProposal_ = p;
@@ -130,13 +132,18 @@ contract Ballot {
         }
     }
 
-    /**
-     * @dev Calls winningProposal() function to get the index of the winner contained in the proposals array and then
-     * @return winnerName_ the name of the winner
-     */
-    function winnerName() public view
-            returns (bytes32 winnerName_)
-    {
+    function getAllProposals() external view returns (Proposal[] memory) {
+        Proposal[] memory items = new Proposal[](proposals.length);
+        for (uint256 i = 0; i < proposals.length; i++) {
+            items[i] = proposals[i];
+        }
+        return items;
+    }
+
+    // Calls winningProposal() function to get the index
+    // of the winner contained in the proposals array and then
+    // returns the name of the winner
+    function winnerName() public view returns (bytes32 winnerName_) {
         winnerName_ = proposals[winningProposal()].name;
     }
 }
